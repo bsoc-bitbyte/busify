7dacb8d1d1401b27d7ada1d256b69f99d7bcd32f
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-05-23 20:27:59 +0530

    project init

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    './node_modules/gts',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'no-console': 1,
+  },
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>init</h1>
+    </>
+  );
+}
+
+export default App;
