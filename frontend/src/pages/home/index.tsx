b54848ccd41980b0e5d3426f851926bfe3c6bde9
Pratik <pratiksolanki2026@gmail.com>
2023-05-31 03:54:22 +0530

    feat(Frontend-button): Added general button component

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,12 +1,18 @@
 import {Typography, Box} from '@mui/material';
 import Navbar from '../../components/Navbar';
 import styles from './home.module.scss';
+// import CustomButton from '../../components/generalButtons/button'
 
 const Home = () => {
+  // const ButtonClicked = () => {
+  //   console.log('Button clicked!');
+  // };
+
   return (
     <Box className={styles.mainWrapper}>
       <Navbar />
       <Typography variant="h4">Home Page</Typography>
+      {/* <CustomButton title='click here to submit' label="submit" size="medium" color="primary" onClick={ButtonClicked} /> */}
     </Box>
   );
 };
--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,1 +1,13 @@
 // Define types for the frontend here
+import {TooltipProps} from '@mui/material/Tooltip';
+
+export interface CustomButtonProps extends Omit<TooltipProps, 'children'> {
+  label: string;
+  variant?: 'contained' | 'outlined' | 'text';
+  icon?: React.ReactNode;
+  disabled?: boolean;
+  color?: 'primary' | 'success' | 'info' | 'warning' | 'error' | 'inherit';
+  size?: 'small' | 'medium' | 'large';
+  href?: string;
+  onClick?: () => void;
+}
