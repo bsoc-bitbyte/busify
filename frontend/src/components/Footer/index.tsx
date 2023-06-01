28f3f3db5601003ab7a18955918a44597807c598
NightFury742 <eshaan122@gmail.com>
2023-06-01 12:57:21 +0530

    refactor(frontedn): index.tsx modified

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,37 +1,38 @@
-import {Typography, useTheme, Grid} from '@mui/material';
+import {Typography, useTheme, Stack} from '@mui/material';
 import Logo from '../../images/iiitdmj-logo.jpg';
 
 export default function Footer() {
   const theme = useTheme();
 
   return (
-    <Grid
-      container
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '0',
-        padding: '0',
-      }}
+    <Stack
+      sx={{justifyContent: 'center', alignItems: 'center'}}
+      direction={{xs: 'column', sm: 'row', md: 'row', lg: 'row', xl: 'row'}}
+      position="static"
+      spacing={0.5}
     >
-      <Grid item p={0.8}>
-        <img
-          src={Logo}
-          alt="College-logo"
-          style={{height: 'calc(8px + 1vw)'}}
-        ></img>
-      </Grid>
-      <Grid item>
-        <Typography
-          variant="body2"
-          color={theme.palette.secondary.main}
-          style={{fontSize: 'calc(4px + 0.5vw)'}}
-          position={'static'}
-        >
-          INDIAN INSTITUTE OF INFORMATION TECHNOLOGY DESIN AND MANUFACTURING
-          JABALPUR
-        </Typography>
-      </Grid>
-    </Grid>
+      <img src={Logo} alt="College-logo" style={{height: '2.5vw'}}></img>
+      <Typography
+        variant="body2"
+        color={theme.palette.secondary.main}
+        style={{fontSize: '1.5vw'}}
+      >
+        INDIAN INSTITUTE OF INFORMATION
+      </Typography>
+      <Typography
+        variant="body2"
+        color={theme.palette.secondary.main}
+        style={{fontSize: '1.5vw'}}
+      >
+        TECHNOLOGY DESIN AND MANUFACTURING
+      </Typography>
+      <Typography
+        variant="body2"
+        color={theme.palette.secondary.main}
+        style={{fontSize: '1.5vw'}}
+      >
+        JABALPUR
+      </Typography>
+    </Stack>
   );
 }
