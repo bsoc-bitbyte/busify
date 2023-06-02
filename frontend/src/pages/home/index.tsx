d7a0ed81c3392c91151e54bb73df3df45d67e3ef
Bhavik Agarwal <73033511+Bhavik-ag@users.noreply.github.com>
2023-06-02 21:47:10 +0530

    fix(frontend):remove unnecessary comments from index.tsx

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -2,19 +2,13 @@ import {Typography, Box} from '@mui/material';
 import Navbar from '../../components/Navbar';
 import styles from './home.module.scss';
 import Footer from '../../components/Footer';
-// import CustomButton from '../../components/CustomButton/index';
 
 const Home = () => {
-  // const ButtonClicked = () => {
-  //   console.log('Button clicked!');
-  // };
-
   return (
     <Box className={styles.mainWrapper}>
       <Navbar />
       <Typography variant="h4">Home Page</Typography>
       <Footer />
-      {/* <CustomButton title='click here to submit' label="submit" size="medium" color="primary"/> */}
     </Box>
   );
 };
