ddc74ececc314cde49c3f8fae61ca508e098ec32
Darpan Mehta <mehtadarpans2004@gmail.com>
2023-06-03 22:16:09 +0530

    feat(frontend): Implemented the changes requested in Navbar and designed the UI as per the Figma requirements

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,81 @@
-import {Typography, Box, Button} from '@mui/material';
+import {Typography, Box, Button, colors} from '@mui/material';
 import {useTheme} from '@mui/material';
 import styles from './navbar.module.scss';
 import getGoogleOAuthURL from '../../utils/getOAuthRedirectUrl';
 import {useAuthStore} from '../../store/authStore';
-
+import google_icon from '../../assets/google_icon.png';
+import help_icon from '../../assets/help_icon.svg';
 export default function Navbar() {
   const theme = useTheme();
   const {isAuth, user} = useAuthStore();
   return (
-    <Box component="nav" className={styles.header}>
-      <Typography variant="h1" color={theme.palette.primary.main}>
-        Busify
+    <Box
+      component="nav"
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Typography
+        variant="h1"
+        color={theme.palette.primary.main}
+        style={{fontSize: 60, marginTop: '56px'}}
+      >
+        BUSIFY
       </Typography>
-      <Typography>A centrailed bus ticket booking system</Typography>
+      <Typography
+        style={{
+          marginLeft: '900px',
+          marginTop: '50px',
+          marginRight: '0px',
+          fontSize: '20px',
+          color: 'rgba(214, 202, 202, 0.788)',
+        }}
+      >
+        <img
+          src={help_icon}
+          style={{
+            height: '20px',
+            width: '20px',
+            marginRight: '5px',
+            marginTop: '30px',
+          }}
+        />
+        Help
+      </Typography>
       {isAuth ? (
-        <Typography>Hi! {user?.name}</Typography>
+        <div
+          style={{
+            borderRadius: '12px',
+            color: 'rgba(0, 0, 0, 0.54)',
+            marginTop: '56px',
+            marginLeft: '0px',
+          }}
+        >
+          <Typography>Hi!, {user?.name}</Typography>
+          <img
+            src=""
+            alt="User Image"
+            style={{width: 30, height: 30, borderRadius: 15}}
+          ></img>
+        </div>
       ) : (
-        <Button variant="contained" color="primary" href={getGoogleOAuthURL()}>
-          Login with Google
+        <Button
+          variant="outlined"
+          color="secondary"
+          href={getGoogleOAuthURL()}
+          style={{
+            borderRadius: '12px',
+            color: 'rgba(0, 0, 0, 0.54)',
+            marginTop: '56px',
+            marginLeft: '0px',
+          }}
+        >
+          <img
+            src={google_icon}
+            style={{height: '40px', marginRight: '40px'}}
+          />
+          Sign in with Google
         </Button>
       )}
     </Box>
