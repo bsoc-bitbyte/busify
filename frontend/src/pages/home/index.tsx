e291738ea7499f31d54410b8d3d86f3743011587
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-06-03 21:53:59 +0530

    fix(frontend): home page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,8 +1,4 @@
-import {Typography, Box, Button} from '@mui/material';
-import Navbar from '../../components/Navbar';
-import styles from './home.module.scss';
-import Footer from '../../components/Footer';
-import {Grid} from '@mui/material';
+import {Box, Button, Grid, Typography, styled} from '@mui/material';
 import bus from '../../assets/bus.png';
 import bgimg from '../../assets/bgimg.png';
 import person from '../../assets/person.png';
@@ -10,177 +6,104 @@ import ticket from '../../assets/ticket.png';
 
 const Home = () => {
   return (
-    <Box className={styles.mainWrapper}>
-      <Navbar />
-
-      <Grid container spacing={10} justifyContent="center" marginTop="1%">
-        <Grid spacing={2} sx={{display: 'flex', flexDirection: 'column'}}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-              position: 'relative',
-            }}
-          >
-            <Box
-              sx={{
-                position: 'relative',
-                display: 'relative',
-                paddingTop: '60px',
-                width: {},
-              }}
-            >
-              <img
-                src={bgimg}
-                alt="bgimg"
-                style={{
-                  width: '200%',
-                  height: '100%',
-                  borderRadius: '15px',
-                }}
-              />
-              <Box
-                position="absolute"
-                top={0}
-                left={0}
-                width="200%"
-                height="90%"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                sx={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.4)',
-                  paddingTop: '100px',
-                  borderRadius: '62px',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  marginTop: '60px',
-                }}
-              >
-                <Typography
-                  sx={{
-                    position: 'absolute',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    top: '50%',
-                    left: '75%',
-                    transform: 'translate(-120%, -70%)',
-                    color: 'white',
-                    fontSize: '4.0rem',
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                  }}
-                >
-                  Effortless transportation for students
-                </Typography>
-
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    position: 'absolute',
-                    height: '50px',
-                    fontWeight: 'bold',
-                    width: '220px',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    top: '50%',
-                    left: '70%',
-                    transform: 'translate(-170%, 250%)',
-                  }}
-                >
-                  See Schedule
-                </Button>
-              </Box>
-            </Box>
-          </Grid>
+    <Grid container direction="column" marginTop="2rem">
+      <Grid item>
+        <HeroContainer>
           <Typography
-            align="center"
-            sx={{fontSize: '40px', fontWeight: 'bold', paddingTop: '70px'}}
+            variant="h2"
+            color="white"
+            fontSize={{xs: '2rem', md: '4rem'}}
+            maxWidth={{xs: '90%', md: '50%'}}
+            textAlign="center"
+            fontWeight="700"
+          >
+            Effortless transportation for students.
+          </Typography>
+          <CTA variant="contained" color="primary">
+            See Schedule
+          </CTA>
+        </HeroContainer>
+        <Box marginBottom="4rem">
+          <Typography
+            variant="h3"
+            fontWeight="700"
+            margin="2rem 0"
+            textAlign="center"
           >
             How to Book
           </Typography>
-        </Grid>
-
-        <Grid item justifyContent="center" marginBottom="8%">
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '250px',
-              width: '250px',
-              boxShadow: '6px 12px 12px rgba(0, 0, 0, 0.25)',
-              borderRadius: '15px',
-              '&:hover': {
-                boxShadow: '2px 1px 4px 4px rgba(0, 0, 0, 0.25)',
-              },
-            }}
-          >
-            <img src={bus} alt="bus" />
-            <Typography sx={{fontWeight: 'bold', paddingTop: '12%'}}>
-              Search For Bus
-            </Typography>
-          </Box>
-        </Grid>
-
-        <Grid item>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '250px',
-              width: '250px',
-              boxShadow: '6px 12px 12px rgba(0, 0, 0, 0.25)',
-              borderRadius: '15px',
-              '&:hover': {
-                boxShadow: '2px 1px 4px 4px rgba(0, 0, 0, 0.25)',
-              },
-            }}
-          >
-            <img src={ticket} alt="ticket" />
-            <Typography sx={{fontWeight: 'bold', paddingTop: '20%'}}>
-              Add Tickets
-            </Typography>
-          </Box>
-        </Grid>
-
-        <Grid item>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '250px',
-              width: '250px',
-              boxShadow: '6px 12px 12px rgba(0, 0, 0, 0.25)',
-              borderRadius: '15px',
-              '&:hover': {
-                boxShadow: '2px 1px 4px 4px rgba(0, 0, 0, 0.25)',
-              },
-            }}
-          >
-            <img src={person} alt="person" />
-            <Typography sx={{fontWeight: 'bold', paddingTop: '20%'}}>
-              Do Payment
-            </Typography>
-          </Box>
-        </Grid>
+          <Grid container spacing={{xs: 4, md: 12}} padding={{md: '0 2rem'}}>
+            <Grid item xs={12} md={4}>
+              <Card>
+                <Box width="100px" height="100px">
+                  <img src={bus} alt="bus" />
+                </Box>
+                <Typography variant="h4" fontWeight="700" marginTop="1rem">
+                  Search for Bus
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card>
+                <Box width="100px" height="100px">
+                  <img src={ticket} alt="ticket" />
+                </Box>
+                <Typography variant="h4" fontWeight="700" marginTop="1rem">
+                  Add Tickets
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card>
+                <Box width="100px" height="100px">
+                  <img src={person} alt="payment" />
+                </Box>
+                <Typography variant="h4" fontWeight="700" marginTop="1rem">
+                  Do Payment
+                </Typography>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
       </Grid>
-
-      <Footer />
-    </Box>
+    </Grid>
   );
 };
+
+const HeroContainer = styled(Box)({
+  background: `linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)), url(${bgimg})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  height: '55vh',
+  padding: '0 1rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 42,
+  marginBottom: '4rem',
+});
+
+const CTA = styled(Button)({
+  marginTop: '2.5rem',
+  borderRadius: 8,
+  padding: '0.5rem 2.5rem',
+  width: 'fit-content',
+  fontSize: '1.2rem',
+});
+
+const Card = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: 'white',
+  justifyContent: 'center',
+  gap: '2rem',
+  alignItems: 'center',
+  boxShadow: '6px 12px 12px rgba(0, 0, 0, 0.25)',
+  borderRadius: '15px',
+  width: '100%',
+  padding: '3rem 1rem',
+});
 
 export default Home;
