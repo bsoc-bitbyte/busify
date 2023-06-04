5268ac45d6d438ef8a9c58f2b0eea45478a245a1
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-06-05 00:07:26 +0530

    refactor(frontend): ticket component

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -2,16 +2,12 @@ import {Typography, Box} from '@mui/material';
 import Navbar from '../../components/Navbar';
 import styles from './home.module.scss';
 import Footer from '../../components/Footer';
-import {TicketData} from '../../components/BusTicket/ticketData';
-import BusTicket from '../../components/BusTicket';
 
 const Home = () => {
   return (
     <Box className={styles.mainWrapper}>
       <Navbar />
       <Typography variant="h4">Home Page</Typography>
-      <BusTicket {...TicketData} />
-      <BusTicket {...TicketData} />
       <Footer />
     </Box>
   );
