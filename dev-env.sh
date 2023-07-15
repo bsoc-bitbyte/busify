0fd0d7bb6bf7daf81b776858e9edf1a360e0f95b
Pratik <pratiksolanki2021@gmail.com>
2023-07-15 14:20:42 +0530

    refactor: remodified the setup.sh and dev-env.sh file to ensure proper development workflow

--- dev-env.sh
+++ dev-env.sh
@@ -6,16 +6,10 @@ sudo docker compose --env-file backend/.env up -d
 ## change directory to backend
 cd backend
 
-## Run migration
-npm run migrate:dev
-
-## Seed the DB
-npm run seed
-
 ## start the backend development server
 npm run start:dev &
 
-## change drectory to frontend
+## change directory to frontend
 cd ../frontend
 
 ##  start the frontend development server
--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+## Run docker Compose
+sudo docker compose --env-file backend/.env up -d
+
 ## change directory to backend
 cd backend
 
 ## install the backend dependencies
 echo "installing backend dependencies..."
 npm i
+
+## Run migration
+npm run migrate:dev
+
+## Seed the DB
+npm run seed
 
 ## change directory to frontend
 cd ../frontend
